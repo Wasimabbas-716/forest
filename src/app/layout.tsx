fa8206121dbe893c48ad12f551163df384f0a860
Wasimabbas-716 <wasimabbas0051@gmail.com>
2024-11-21 16:37:32 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <head>
-     {process.env.NODE_ENV === "production" && (
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1430143994998751"
-     crossOrigin="anonymous"></script>
-     )}
-     </head>
+ 
       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
